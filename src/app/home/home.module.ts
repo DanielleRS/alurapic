82b4388d6204744c08f829d2d2aa560191bed767
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-10-19 13:44:31 -0300

    Validacao de formularios

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
 import { SignInComponent } from "./signin/signin.component";
 
 @NgModule({
-    declarations: [ SignInComponent ]
+    declarations: [ SignInComponent ],
+    imports: [ 
+        ReactiveFormsModule,
+        CommonModule
+    ]
 })
 export class HomeModule { }
