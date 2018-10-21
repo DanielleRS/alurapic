44831cd023b2116205c99c58aad684a96f1b5427
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-10-21 18:57:20 -0300

    Obtendo dados do formulario

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -9,6 +9,8 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
+  file: File;
+
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
@@ -17,5 +19,10 @@ export class PhotoFormComponent implements OnInit {
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
     })
+  }
+
+  upload() {
+    const description = this.photoForm.get('description');
+    const allowComments = this.photoForm.get('allowComments');
   }
 }
