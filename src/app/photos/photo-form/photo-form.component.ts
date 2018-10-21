4d3e71dc783a507748a286e09939d25bce5e3cd9
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-10-21 19:37:49 -0300

    Preview de imagem

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -12,6 +12,7 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -32,5 +33,12 @@ export class PhotoFormComponent implements OnInit {
     const allowComments = this.photoForm.get('allowComments');
     this.photoService.upload(description, allowComments, this.file)
                     .subscribe(() => this.router.navigate(['']))
+  }
+
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 }
