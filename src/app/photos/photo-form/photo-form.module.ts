dca1e0267059ef75806324873bd0c92e107229a3
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-10-21 18:27:39 -0300

    Formulario upload de fotos

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
 import { PhotoFormComponent } from "./photo-form.component";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { VMessageComponent } from "../../shared/components/vmessage/vmessage.component";
 
 @NgModule({
     declarations: [
         PhotoFormComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule,
+        FormsModule
     ]
 })
 export class PhotoFormModule {}
