b716eabd5d4567a057cb06867e62bdc6584415e8
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-10-21 19:11:16 -0300

    Upload da foto

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PhotoService } from '../photo/photo.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ap-photo-form',
@@ -11,18 +13,24 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private photoService: PhotoService,
+    private router: Router
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.photoForm = this.formBuilder.group({
       file: ['', Validators.required],
       description: ['', Validators.maxLength(300)],
       allowComments: [true]
-    })
+    });
   }
 
   upload() {
     const description = this.photoForm.get('description');
     const allowComments = this.photoForm.get('allowComments');
+    this.photoService.upload(description, allowComments, this.file)
+                    .subscribe(() => this.router.navigate(['']))
   }
 }
