d8e80f64aa3f122d13eafa0ba520b2f4ab4a133c
Danielle Rodrigues <30360153+DanielleRS@users.noreply.github.com>
2018-10-25 11:56:30 -0300

    Indicador de trabalho

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -4,6 +4,7 @@ import { PhotoService } from '../photo/photo.service';
 import { Router } from '@angular/router';
 import { AlertService } from '../../shared/components/alert/alert.service';
 import { UserService } from '../../core/user/user.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'ap-photo-form',
@@ -15,6 +16,7 @@ export class PhotoFormComponent implements OnInit {
   photoForm: FormGroup;
   file: File;
   preview: string;
+  percentDone = 0;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -36,9 +38,13 @@ export class PhotoFormComponent implements OnInit {
     const description = this.photoForm.get('description').value;
     const allowComments = this.photoForm.get('allowComments').value;
     this.photoService.upload(description, allowComments, this.file)
-                    .subscribe(() => {
-                      this.alertService.success('Upload complete', true);
-                      this.router.navigate(['/user', this.userService.getUserName()]);
+                    .subscribe((event: HttpEvent<any>) => {
+                      if(event.type == HttpEventType.UploadProgress) {
+                        this.percentDone = Math.round(100 * event.loaded / event.total);
+                      } else if(event.type == HttpEventType.Response) {
+                          this.alertService.success('Upload complete', true);
+                          this.router.navigate(['/user', this.userService.getUserName()]);
+                      }
                     });
   }
 
